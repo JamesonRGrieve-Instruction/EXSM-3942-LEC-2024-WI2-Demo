6665bb46bfca32827bd6c996d00079226e46b78c
James Grieve <jamesonrgrieve@gmail.com>
2024-02-27 19:30:16 -0700

    Class definition with public fields and properties;

--- src/DemoProject/Program.cs
+++ src/DemoProject/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Person person = new Person();
+        person.FirstName = "John";
+        person.MiddleName = "Test";
+        person.LastName = "Doe";
+        Console.WriteLine($"Person: {person.FirstName} {person.MiddleName} {person.LastName}");
+        Console.WriteLine("Program end...");
     }
+}
+public class Person
+{
+    public string? FirstName;
+    private string? _middleName;
+    public string? MiddleName
+    {
+        get
+        {
+            return _middleName;
+        }
+        set
+        {
+            _middleName = value;
+        }
+    }
+    public string? LastName
+    {
+        get; set;
+    }
+
 }
