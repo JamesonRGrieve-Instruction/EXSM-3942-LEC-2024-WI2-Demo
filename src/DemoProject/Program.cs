ad89ffb4278ae6b76b6599f002161d32464b6f7e
James Grieve <jamesonrgrieve@gmail.com>
2024-02-27 19:37:35 -0700

    Derived property;

--- src/DemoProject/Program.cs
+++ src/DemoProject/Program.cs
@@ -8,7 +8,10 @@ class Program
         person.FirstName = "John";
         person.MiddleName = "Test";
         person.LastName = "Doe";
+        // We cannot assign to derived properties as they are read-only (unless a setter is implemented).
+        //person.FullName = "Test McTest";
         Console.WriteLine($"Person: {person.FirstName} {person.MiddleName} {person.LastName}");
+        Console.WriteLine($"Full Name: {person.FullName}");
         Console.WriteLine("Program end...");
     }
 }
@@ -30,6 +33,13 @@ public class Person
     public string? LastName
     {
         get; set;
+    }
+    public string FullName
+    {
+        get
+        {
+            return $"{this.FirstName} {this.MiddleName} {this.LastName}";
+        }
     }
 
 }
