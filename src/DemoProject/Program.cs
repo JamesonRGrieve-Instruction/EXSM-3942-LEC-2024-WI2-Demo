01c81ba0c819eae3e8f904fcd2d344a58ae14432
James Grieve <jamesonrgrieve@gmail.com>
2024-02-29 19:29:04 -0700

    Derived property example;

--- src/DemoProject/Program.cs
+++ src/DemoProject/Program.cs
@@ -3,46 +3,73 @@
 class Program
 {
     static void Main(string[] args)
+    // Entry Point (Start)
     {
+        // Instantiation
         Pen pen = new Pen("Bic", "Blue", 80f);
+        // Method Call
         pen.Write(100);
         pen.Write(42);
         pen.Write(200);
+        // Output
         Console.WriteLine($"The pen has {pen.InkLevel:0.00}mL of ink remaining, {pen.CalcRemainingInkPercentage():0.00}% ink remaining.");
+        // Exit Point (End)
     }
 }
+// Class Definition
 public class Pen
 {
-    public Pen(string brand, string colour, float maxInk)
+    // Constructor Definition
+    public Pen(string brand /*Constructor Parameters*/, string colour, float maxInk)
     {
         this.Brand = brand;
         this.Colour = colour;
         this.InkLevel = maxInk;
         this.MaxInk = maxInk;
     }
+    // Auto-Implemented Property
     public string Brand { get; set; }
     public string Colour { get; set; }
+    // Auto-Implemented Property w/ Private Setter
     public float MaxInk { get; private set; }
+    // Private Field
     private float _inkLevel;
+    // Fully Implemented Property
     public float InkLevel
     {
+        // Accessor
         get
         {
             return _inkLevel;
         }
+        // Mutator (Private)
         private set
         {
+            // Validation
             if (value < 0)
             {
                 throw new Exception("InkLevel cannot be set to a negative value.");
             }
+            // 'value' is a keyword representing the inbound value.
             this._inkLevel = value;
         }
     }
-    public float CalcRemainingInkPercentage()
+
+    // Derived Property
+    public float RemainingInkPercentage
     {
-        return (float)Math.Round(InkLevel / MaxInk * 100, 2);
+        get
+        {
+            return (float)Math.Round(InkLevel / MaxInk * 100, 2);
+        }
     }
+
+    // Method (Function) Definitions
+    // public float CalcRemainingInkPercentage()
+    // {
+    //     return (float)Math.Round(InkLevel / MaxInk * 100, 2);
+    // }
+
     public void Write(int characters)
     {
         try
