2432544289ad4eab8d57f22a30b7143349de3f44
James Grieve <jamesonrgrieve@gmail.com>
2024-02-29 19:19:42 -0700

    Implement discrete ink volume;

--- src/DemoProject/Program.cs
+++ src/DemoProject/Program.cs
@@ -4,23 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Pen pen = new Pen("Bic", "Blue");
+        Pen pen = new Pen("Bic", "Blue", 80f);
         pen.Write(100);
         pen.Write(42);
         pen.Write(200);
-        Console.WriteLine($"The pen has ${pen.InkLevel}% ink remaining.");
+        Console.WriteLine($"The pen has {pen.InkLevel:0.00}mL of ink remaining, {pen.CalcRemainingInkPercentage():0.00}% ink remaining.");
     }
 }
 public class Pen
 {
-    public Pen(string brand, string colour)
+    public Pen(string brand, string colour, float maxInk)
     {
         this.Brand = brand;
         this.Colour = colour;
-        this.InkLevel = 100f;
+        this.InkLevel = maxInk;
+        this.MaxInk = maxInk;
     }
     public string Brand { get; set; }
     public string Colour { get; set; }
+    public float MaxInk { get; private set; }
     private float _inkLevel;
     public float InkLevel
     {
@@ -37,12 +39,15 @@ public class Pen
             this._inkLevel = value;
         }
     }
-
+    public float CalcRemainingInkPercentage()
+    {
+        return (float)Math.Round(InkLevel / MaxInk * 100, 2);
+    }
     public void Write(int characters)
     {
         try
         {
-            InkLevel -= characters * 0.5f;
+            InkLevel -= characters * 0.1f;
         }
         catch (Exception exception)
         {
