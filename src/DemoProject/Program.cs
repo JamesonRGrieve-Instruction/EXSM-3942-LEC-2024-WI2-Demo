2631253c67631f508125c5fd2ad62a6fc792fa44
James Grieve <jamesonrgrieve@gmail.com>
2024-03-12 19:44:28 -0600

    Queue of tasks with workers;

--- src/DemoProject/Program.cs
+++ src/DemoProject/Program.cs
@@ -2,8 +2,34 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Queue<Func<int, Task<int>>> tasks = new Queue<Func<int, Task<int>>>();
+        for (int i = 1; i <= 10; i++)
+        {
+            tasks.Enqueue(WaitForSecondsAsync);
+        }
+
+        Task[] workers = {
+            CreateWorker(tasks),
+            CreateWorker(tasks),
+            CreateWorker(tasks),
+        };
+        Task.WaitAll(workers);
+    }
+    static async Task CreateWorker(Queue<Func<int, Task<int>>> taskQueue)
+    {
+        while (taskQueue.Count > 0)
+        {
+            await taskQueue.Dequeue()(1);
+        }
+
+    }
+    static async Task<int> WaitForSecondsAsync(int seconds)
+    {
+        Console.WriteLine($"Waiting for {seconds} seconds.");
+        await Task.Delay(seconds * 1000);
+        Console.WriteLine($"Waited for {seconds} seconds.");
+        return seconds;
     }
 }
