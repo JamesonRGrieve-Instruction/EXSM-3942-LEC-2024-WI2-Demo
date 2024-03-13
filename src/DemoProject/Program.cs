0edc7f68f694f892a6827c0365a3ffb10c38688c
James Grieve <jamesonrgrieve@gmail.com>
2024-03-12 20:28:12 -0600

    Request and output data from API;

--- src/DemoProject/Program.cs
+++ src/DemoProject/Program.cs
@@ -10,10 +10,37 @@ class Program
     static readonly HttpClient client = new HttpClient();
     static async Task Main(string[] args)
     {
-        Console.Write("Please enter a Pokemon ID: ");
-        int pokemonID = int.Parse(Console.ReadLine());
-        PokeDTO found = await FetchPokemon(pokemonID);
-        Console.WriteLine(found.Name);
+        List<PokeDTO> cachedPokemon = new List<PokeDTO>();
+        int userChoice = 1;
+        do
+        {
+            Console.Write("Pokemon Information\n1. Retrieve Pokemon\n2. Exit\n\tChoice: ");
+            userChoice = int.Parse(Console.ReadLine());
+            if (userChoice == 1)
+            {
+                Console.Write("Enter a Pokemon ID: ");
+                int pokemonID = int.Parse(Console.ReadLine());
+                try
+                {
+                    PokeDTO found;
+                    if (cachedPokemon.Any(pokemon => pokemon.ID == pokemonID))
+                    {
+                        found = cachedPokemon.Single(pokemon => pokemon.ID == pokemonID);
+                    }
+                    else
+                    {
+                        found = await FetchPokemon(pokemonID);
+                        cachedPokemon.Add(found);
+                    }
+                    Console.WriteLine($"-- {found.Name} --\nTypes: {string.Join(",", found.Types.Select(type => type.Type.Name))}\nWeight: {found.Weight}\nHeight: {found.Height}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("ERROR: " + e.Message);
+                }
+            }
+        } while (userChoice != 2);
+
     }
     static async Task<PokeDTO> FetchPokemon(int id)
     {
@@ -30,10 +57,20 @@ class PokeDTO
     public int ID { get; set; }
     [JsonPropertyName("name")]
     public string Name { get; set; }
-    [JsonPropertyName("types[]")]
-    public string[] Types { get; set; }
+    [JsonPropertyName("types")]
+    public PokeTypeWrapper[] Types { get; set; }
     [JsonPropertyName("height")]
     public int Height { get; set; }
     [JsonPropertyName("weight")]
     public int Weight { get; set; }
+}
+class PokeTypeWrapper
+{
+    [JsonPropertyName("type")]
+    public PokeTypeDTO Type { get; set; }
+}
+class PokeTypeDTO
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
 }
