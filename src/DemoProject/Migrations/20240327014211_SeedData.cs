f99d35a2f5136170b5e17d0594b172818221616d
James Grieve <jamesonrgrieve@gmail.com>
2024-03-26 19:43:32 -0600

    Seed data;

---
+++ src/DemoProject/Migrations/20240327014211_SeedData.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "program",
+                columns: new[] { "id", "name" },
+                values: new object[] { -1, "Fullstack Web Application Development" });
+
+            migrationBuilder.InsertData(
+                table: "semester",
+                columns: new[] { "id", "end_date", "name", "start_date" },
+                values: new object[] { -1, new DateOnly(2024, 4, 26), "Winter 2024", new DateOnly(2024, 1, 8) });
+
+            migrationBuilder.InsertData(
+                table: "student",
+                columns: new[] { "id", "birthdate", "firstname", "lastname", "student_id" },
+                values: new object[] { -1, new DateOnly(1990, 1, 1), "John", "Doe", null });
+
+            migrationBuilder.InsertData(
+                table: "course",
+                columns: new[] { "id", "code", "name", "program_id" },
+                values: new object[] { -1, "EXSM3931", "Introduction to Web Development", -1 });
+
+            migrationBuilder.InsertData(
+                table: "section",
+                columns: new[] { "id", "course_id", "days_offered", "semester_id", "time_offered" },
+                values: new object[] { -1, -1, "MR", -1, new TimeOnly(19, 0, 0) });
+
+            migrationBuilder.InsertData(
+                table: "enrollment",
+                columns: new[] { "id", "grade", "section_id", "student_id" },
+                values: new object[] { -1, null, -1, -1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "enrollment",
+                keyColumn: "id",
+                keyValue: -1);
+
+            migrationBuilder.DeleteData(
+                table: "section",
+                keyColumn: "id",
+                keyValue: -1);
+
+            migrationBuilder.DeleteData(
+                table: "student",
+                keyColumn: "id",
+                keyValue: -1);
+
+            migrationBuilder.DeleteData(
+                table: "course",
+                keyColumn: "id",
+                keyValue: -1);
+
+            migrationBuilder.DeleteData(
+                table: "semester",
+                keyColumn: "id",
+                keyValue: -1);
+
+            migrationBuilder.DeleteData(
+                table: "program",
+                keyColumn: "id",
+                keyValue: -1);
+        }
+    }
+}
--- src/DemoProject/Models/EducationContext.cs
+++ src/DemoProject/Models/EducationContext.cs
@@ -43,6 +43,15 @@ public partial class EducationContext : DbContext
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
             entity.HasOne(d => d.Program).WithMany(p => p.Courses).HasConstraintName("course_ibfk_1");
+
+            entity.HasData([
+                new Course() {
+                    Id = -1,
+                    Code = "EXSM3931",
+                    Name = "Introduction to Web Development",
+                    ProgramId = -1
+                }
+            ]);
         });
 
         modelBuilder.Entity<Enrollment>(entity =>
@@ -52,11 +61,27 @@ public partial class EducationContext : DbContext
             entity.HasOne(d => d.Section).WithMany(p => p.Enrollments).HasConstraintName("enrollment_ibfk_2");
 
             entity.HasOne(d => d.Student).WithMany(p => p.Enrollments).HasConstraintName("enrollment_ibfk_1");
+
+            entity.HasData([
+                new Enrollment() {
+                    Id = -1,
+                    StudentId = -1,
+                    SectionId = -1,
+                    Grade = null
+                }
+            ]);
         });
 
         modelBuilder.Entity<Program>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.HasData([
+                new Program() {
+                    Id = -1,
+                    Name = "Fullstack Web Application Development",
+                }
+            ]);
         });
 
         modelBuilder.Entity<Section>(entity =>
@@ -66,16 +91,44 @@ public partial class EducationContext : DbContext
             entity.HasOne(d => d.Course).WithMany(p => p.Sections).HasConstraintName("section_ibfk_1");
 
             entity.HasOne(d => d.Semester).WithMany(p => p.Sections).HasConstraintName("section_ibfk_2");
+
+            entity.HasData([
+                new Section() {
+                    Id = -1,
+                    CourseId = -1,
+                    SemesterId = -1,
+                    DaysOffered = "MR",
+                    TimeOffered = new TimeOnly(19, 0)
+                }
+            ]);
         });
 
         modelBuilder.Entity<Semester>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.HasData([
+                new Semester() {
+                    Id = -1,
+                    Name = "Winter 2024",
+                    StartDate = new DateOnly(2024, 1, 8),
+                    EndDate = new DateOnly(2024, 4, 26)
+                }
+            ]);
         });
 
         modelBuilder.Entity<Student>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.HasData([
+                new Student() {
+                    Id = -1,
+                    Firstname = "John",
+                    Lastname = "Doe",
+                    Birthdate = new DateOnly(1990,1,1)
+                }
+            ]);
         });
 
         OnModelCreatingPartial(modelBuilder);
